90be001412f3a152173865d4d5b5b1af2e8a3bc8
Mladen Stepanic <crawleyprint@gmail.com>
2024-09-27 22:38:35 +0200

    Neogit and colorscheme setup

--- init.lua
+++ init.lua
@@ -279,7 +279,12 @@ require('lazy').setup({
       'ibhagwan/fzf-lua', -- optional
       'echasnovski/mini.pick', -- optional
     },
-    config = true,
+    config = function()
+      require('neogit').setup {
+        graph_style = 'unicode',
+        diffview = true,
+      }
+    end,
     vim.keymap.set('n', '<leader>g', ':Neogit<CR>'),
   },
 
@@ -851,21 +856,35 @@ require('lazy').setup({
     end,
   },
 
+  -- NightFox colorscheme
+  -- https://github.com/EdenEast/nightfox.nvim
+  {
+    'EdenEast/nightfox.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      require('nightfox').setup {
+        options = {
+          terminal_colors = true,
+        },
+      }
+      vim.cmd 'colorscheme duskfox'
+    end,
+  },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-storm'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 
